d0d09d7e09681d35842d85e79cc69b85e64c8aa3
marxama <marxama@gmail.com>
2012-12-14 00:20:42 +0100

    Started adding some tests

---
+++ test/project_euler/utils_test.clj
@@ -0,0 +1,7 @@
+(ns project-euler.utils-test
+  (:use clojure.test
+        project-euler.utils
+        project-euler.test-constants))
+
+(deftest test-primes
+  (is (= first-1000-primes (take 1000 (primes)))))
