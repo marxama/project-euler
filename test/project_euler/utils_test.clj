9cc17fc0d382535bc671065b06518f1c6ee6487c
marxama <marxama@gmail.com>
2012-12-31 00:32:50 +0100

    Lots and lots of stuff added. Don't know what happened with the diffs!

--- test/project_euler/utils_test.clj
+++ test/project_euler/utils_test.clj
@@ -20,6 +20,51 @@
   (is (= true (pandigital? 1)))
   (is (= true (pandigital? 578249631))))
 
+(deftest test-triangulars
+  (is (= '(1 3 6 10 15 21 28 36 45 55)
+         (take 10 (triangulars)))))
+
+(deftest test-triangular?
+  (is (nil? (->> (triangulars)
+              (take 1000)
+              (map triangular?)
+              (some false?)))))
+
 (deftest test-pentagonals
-  (is (= '(1 5 12 22 35 51 70 92 117 145)
-         (take 10 (pentagonals)))))
+  (is (= '(1 5 12 22 35 51 70 92 117 145 176 210 247 287 330 376 425 477 532 590 651 715 782 852 925 1001) 
+         (take 26 (pentagonals)))))
+
+(deftest test-pentagonal?
+  (is (nil? (->> (pentagonals)
+              (take 1000)
+              (map pentagonal?)
+              (some false?)))))
+
+(deftest test-hexagonals
+  (is (= '(1 6 15 28 45 66 91 120 153 190 231 276 325 378 435 496 561 630 703 780 861 946)
+         (take 22 (hexagonals)))))
+
+(deftest test-hexagonal?
+  (is (nil? (->> (hexagonals)
+              (take 1000)
+              (map hexagonal?)
+              (some false?)))))
+
+(deftest test-odd-composites
+  (is (= '(9 15 21 25 27 33 35 39 45 49)
+         (take 10 (odd-composites)))))
+
+(deftest test-r-from-coll
+  (is (= #{#{1 2} #{1 3} #{2 3}}
+         (into #{} (map (partial into #{}) (r-from-coll 2 [1 2 3])))))
+  (is (= 15504 (count (r-from-coll 5 (range 20))))))
+
+(deftest test-count-r-from-n
+  (is (= 2598960 (count-r-from-n 5 52))))
+
+(deftest test-permutations
+  (is (= #{[1 2 3] [1 3 2] [2 1 3] [2 3 1] [3 1 2] [3 2 1]}
+         (into #{} (permutations [1 2 3]))))
+  (is (= [[1 2 2] [1 2 2] [2 1 2] [2 2 1] [2 1 2] [2 2 1]]
+         (permutations [1 2 2])))
+  (is (= (! 8) (count (permutations (range 8))))))
