2b0525c33cbc2e8c8348590129d121382c3c3030
marxama <marxama@gmail.com>
2013-01-02 22:39:46 +0100

    Problem 56 added

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -494,3 +494,14 @@ How many Lychrel numbers are there below ten-thousand?"
            (map bigint)
            (filter lychrel?)
            count)))
+
+(defn problem-56
+"A googol (10^100) is a massive number: one followed by one-hundred zeros; 100^100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+
+Considering natural numbers of the form, ab, where a, b  100, what is the maximum digital sum?"
+  []
+  (apply max
+         (apply concat
+                (for [a (range 1 100)]
+                  (map (comp (partial apply +) utils/digits)
+                       (reductions (fn [acc _] (* acc a)) 1N (range 1 100)))))))
